57cf476753f665b689647a7504f5a10c58f6a058
tetris245 <58624501+tetris245@users.noreply.github.com>
2022-05-03 16:25:16 +0200

    Removed boot up sequence of Moaner script

--- Quick-AccessMenu2.1/script.js
+++ Quick-AccessMenu2.1/script.js
@@ -3498,45 +3498,6 @@ expressionAnimation[expressionAnimation.length-1]=expressionAnimationEx.Property
 //core functionaliy moved to: /speak /mouth /speech
 
 //Moaner
-//Boot up sequence
-
-
-window.addEventListener("load", () => {
-
-  function M_MOANER_AddExternalScript(scriptLink) {
-	let script = document.createElement("script");
-	script.src = scriptLink;
-	script.src.async = false;
-	return document.head.appendChild(script);
-  }
-  
-  function M_MOANER_AddScript(scriptFileName) {
-	let script = document.createElement("script");
-	script.src = chrome.runtime.getURL(scriptFileName);
-	return document.head.appendChild(script);
-  }
-  
-  const M_MOANER_externalScripts = [];
-  
-  
-  const M_MOANER_scripts = [
-	"js/system/util.js",
-	"js/system/controls.js",
-	"js/Reactions/moansManagement.js",
-	"js/system/ChatRoom.js",
-	"js/Reactions/Reactions.js",
-	"js/moans.js"
-  ];
-  
-  M_MOANER_externalScripts.forEach(M_MOANER_AddExternalScript);
-  M_MOANER_scripts.forEach(M_MOANER_AddScript);
-  
-
-});
-
-
-
-
 //ChatRoom
 /*var backupChatRoomSendChat;
 var backupActivityOrgasmPrepare;
