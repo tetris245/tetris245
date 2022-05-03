c2ff0dca9dadb29acc8de314dbb9a6c832d8b1f1
tetris245 <58624501+tetris245@users.noreply.github.com>
2022-05-03 15:28:17 +0200

    runtime.getURL instead of extension.getURL

--- Quick-AccessMenu2.1/contentscript.js
+++ Quick-AccessMenu2.1/contentscript.js
@@ -1,5 +1,5 @@
 var s = document.createElement('script');
-s.src = chrome.extension.getURL('script.js');
+s.src = chrome.runtime.getURL('script.js');
 (document.head || document.documentElement).appendChild(s);
 // s.onload = function () {
 //   s.parentNode.removeChild(s);
