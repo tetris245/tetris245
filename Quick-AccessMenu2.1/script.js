63435a954fbfe1a7a15be42733d9eb4917f54728
tetris245 <58624501+tetris245@users.noreply.github.com>
2022-05-04 08:56:13 +0200

    Moans bugfix

--- Quick-AccessMenu2.1/script.js
+++ Quick-AccessMenu2.1/script.js
@@ -4564,10 +4564,10 @@ function IsStimulated(C){
 //addMoansProfile("neko",nekoMoans);
 
 M_MOANER_nekoMoans={
-	"hot":["n... Nyahâ™¥","NYyaaAâ™¥"],
-	"medium":["nyAhâ™¥","nyyy","..yah"],
-	"light":["nyahâ™¥","Yah!","myuh","mhâ™¥"],
-	"low":["myu","nyâ™¥","mh","â™¥yhâ™¥","nyâ™¥"],
+	"hot":["n... Nyah♥","NYyaaA♥"],
+	"medium":["nyAh♥","nyyy","..yah"],
+	"light":["nyah♥","Yah!","myuh","mh♥"],
+	"low":["myu","ny♥","mh","♥yh♥","ny♥"],
 	"orgasm":["Nya...Ny...NyaaAAaah!","Mmmhnn... Nyhmm... Nyah!","mmmh... mmmeeeee.... meeeoooow!"],
 	"pain":[]
 }
@@ -4578,23 +4578,23 @@ M_MOANER_addMoansProfile("neko",M_MOANER_nekoMoans);
 //base: wif, yif, aouh
 //thanks to Noriko
 M_MOANER_foxMoans={
-	"hot":["w... Wiiifâ™¥","Yiiifâ™¥"],
-	"medium":["wiiifâ™¥","Yiii","..yif"],
-	"light":["Wiffâ™¥","Yif!","yiâ™¥iif","Wiif"],
-	"low":["wif","Wyâ™¥","ifâ™¥","â™¥yiâ™¥","Yiâ™¥"],
-	"orgasm":["Wiffâ™¥ W... Wiii... WIIF!!","Mmmhnn... Wiiif... Yiiiif!!","mmmh... Aouuuh.... Aouhhhh!"],
+	"hot":["w... Wiiif♥","Yiiif♥"],
+	"medium":["wiiif♥","Yiii","..yif"],
+	"light":["Wiff♥","Yif!","yi♥iif","Wiif"],
+	"low":["wif","Wy♥","if♥","♥yi♥","Yi♥"],
+	"orgasm":["Wiff♥ W... Wiii... WIIF!!","Mmmhnn... Wiiif... Yiiiif!!","mmmh... Aouuuh.... Aouhhhh!"],
 	"pain":[]
 }
 M_MOANER_addMoansProfile("fox",M_MOANER_foxMoans);
 
 //dog
 M_MOANER_dogMoans={
-	"hot":["w... Wouuufâ™¥","aouuhâ™¥"],
-	"medium":["waaafâ™¥","kyÅ«Å«Å«n","..wouf"],
-	"light":["Ouaffâ™¥","Aouh!","Ouaâ™¥af","KyÅ«nâ™¥"],
-	"low":["wou..","ouahâ™¥","Woufâ™¥","â™¥kyÅ«nâ™¥","kyÅ«â™¥"],
-	"orgasm":["ouafâ™¥ O... Ouuw... Ouaaaa!!","Mmmhnn... aaaa... Ouuuaaaaaf!!","mmmh... Aouuuh.... Aouhhhh!"],
-	"pain":["KaÃ¯!","Aoouch!","KaaaÃ¯!","Ouch","Aow"]
+	"hot":["w... Wouuuf♥","aouuh♥"],
+	"medium":["waaaf♥","kyūūūn","..wouf"],
+	"light":["Ouaff♥","Aouh!","Oua♥af","Kyūn♥"],
+	"low":["wou..","ouah♥","Wouf♥","♥kyūn♥","kyū♥"],
+	"orgasm":["ouaf♥ O... Ouuw... Ouaaaa!!","Mmmhnn... aaaa... Ouuuaaaaaf!!","mmmh... Aouuuh.... Aouhhhh!"],
+	"pain":["Kaï!","Aoouch!","Kaaaï!","Ouch","Aow"]
 }
 M_MOANER_addMoansProfile("dog",M_MOANER_dogMoans);
 
@@ -4602,20 +4602,20 @@ M_MOANER_addMoansProfile("dog",M_MOANER_dogMoans);
 //base coui
 
 M_MOANER_mouseMoans={
-	"hot":["Scouiiicâ™¥","couiiicâ™¥"],
-	"medium":["scouiiiâ™¥","Couyk","..scoui"],
-	"light":["Scouiiâ™¥","Coui!","kouuâ™¥ic","Couic â™¥"],
-	"low":["coui..","scouiâ™¥","couâ™¥i","Couic ","kouiâ™¥"],
-	"orgasm":["Couicâ™¥ sc.. couIIIiic!!","Mmmhnn... ooo... ouiiiic!!","mmmh... Scouuu.... Scouiiic!"],
+	"hot":["Scouiiic♥","couiiic♥"],
+	"medium":["scouiii♥","Couyk","..scoui"],
+	"light":["Scouii♥","Coui!","kouu♥ic","Couic ♥"],
+	"low":["coui..","scoui♥","cou♥i","Couic ","koui♥"],
+	"orgasm":["Couic♥ sc.. couIIIiic!!","Mmmhnn... ooo... ouiiiic!!","mmmh... Scouuu.... Scouiiic!"],
 	"pain":[]
 }
 M_MOANER_addMoansProfile("mouse",M_MOANER_mouseMoans);
 
 M_MOANER_wildFoxMoans={
-	"hot":["w... Wiiifâ™¥","Yiiifâ™¥","Waâ™¥ouu"],
-	"medium":["wiiifâ™¥","Yiii","..yif","waouuu"],
-	"light":["Wiffâ™¥","Yif!","yiâ™¥iif","Wiif","waou"],
-	"low":["wif","Wyâ™¥","ifâ™¥","â™¥yiâ™¥","Yiâ™¥","aou"],
+	"hot":["w... Wiiif♥","Yiiif♥","Wa♥ouu"],
+	"medium":["wiiif♥","Yiii","..yif","waouuu"],
+	"light":["Wiff♥","Yif!","yi♥iif","Wiif","waou"],
+	"low":["wif","Wy♥","if♥","♥yi♥","Yi♥","aou"],
 	"orgasm":["WAAAAOUUUUUUUHHHHH!","Mmmhnn... Wiiif... Yiiiif!!","AOUUUUUH!","WAHOOOOOOOUUUUH!","WAAAAAAAAHH!","WAAAAOUUUUUUUHHHHH!","AOUUUUUH!","WAHOOOOOOOUUUUH!","WAAAAAAAAHH!"],
 	"pain":[]
 }
@@ -4623,11 +4623,11 @@ M_MOANER_addMoansProfile("wildFox",M_MOANER_wildFoxMoans);
 
 //pig
 M_MOANER_pigMoans={
-	"hot":["Gruiikâ™¥","gruikâ™¥"],
-	"medium":["gruiiiâ™¥","Gruik","..Grui.."],
-	"light":["Gruiâ™¥","Gruik!","gruuiiiâ™¥ic","gruik â™¥"],
-	"low":["grui.. gruiikâ™¥","gruiikâ™¥","gruâ™¥i","Gruik ","Groiâ™¥"],
-	"orgasm":["Gruâ™¥ gr.. gruiIIIiick!!","Mmmhnn... uii... gruiiik!!","mmmh... Gruiik.... Gruiiiiik!"],
+	"hot":["Gruiik♥","gruik♥"],
+	"medium":["gruiii♥","Gruik","..Grui.."],
+	"light":["Grui♥","Gruik!","gruuiii♥ic","gruik ♥"],
+	"low":["grui.. gruiik♥","gruiik♥","gru♥i","Gruik ","Groi♥"],
+	"orgasm":["Gru♥ gr.. gruiIIIiick!!","Mmmhnn... uii... gruiiik!!","mmmh... Gruiik.... Gruiiiiik!"],
 	"pain":["Gruuik!!","Aoouch!","Awo... gruik!","Ouch","Gruiiik"]
 }
 M_MOANER_addMoansProfile("pig",M_MOANER_pigMoans);
