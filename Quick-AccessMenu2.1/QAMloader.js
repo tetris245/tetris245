39262fc7c9468b56ddb396416c8cfbf110a18b8f
tetris245 <58624501+tetris245@users.noreply.github.com>
2022-06-04 06:22:11 +0200

    Create QAMloader.js

---
+++ Quick-AccessMenu2.1/QAMloader.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         Quick-AccessMenu2.1
+// @namespace    https://www.bondageprojects.com/
+// @version      1.4.1
+// @description  Everything you'll ever need for BC
+// @author       Nemesea
+// @match https://bondageprojects.elementfx.com/*
+// @match https://www.bondageprojects.elementfx.com/*
+// @match https://bondage-europe.com/*
+// @match https://www.bondage-europe.com/*
+// @match http://localhost:*/*
+// @icon data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant none
+// @run-at document-end
+// ==/UserScript==
+
+setTimeout(
+    function() {
+        'use strict';
+
+        let n = document.createElement("script");
+        n.setAttribute("language", "JavaScript");
+        n.setAttribute("crossorigin", "anonymous");
+        n.setAttribute("src", "https://github.com/tetris245/tetris245/blob/master/Quick-AccessMenu2.1/script.js?_=" + Date.now());
+        n.onload = () => n.remove();
+        document.head.appendChild(n);
+    },
+    2000
+);
