40f15aa242a99495c692e344c39538879932f6f8
tetris245 <58624501+tetris245@users.noreply.github.com>
2022-06-04 07:46:42 +0200

    Update script.js

--- Quick-AccessMenu2.1/script.js
+++ Quick-AccessMenu2.1/script.js
@@ -1,20 +1,3 @@
-
-// ==UserScript==
-// @name Quick-AccessMenu2.1
-// @namespace https://www.bondageprojects.com/
-// @version 1.4.1
-// @description Everything you'll ever need for BC
-// @author Nemesea
-// @match https://bondageprojects.elementfx.com/*
-// @match https://www.bondageprojects.elementfx.com/*
-// @match https://bondage-europe.com/*
-// @match https://www.bondage-europe.com/*
-// @match http://localhost:*/*
-// @icon data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
-// @grant none
-// @run-at document-end
-// ==/UserScript==
-
 (typeof OLDmenu !== "undefined") && (ChatRoomSendChat = OLDmenu);//reset
 function NEWmenu() {
 var content = ElementValue("InputChat").trim();
