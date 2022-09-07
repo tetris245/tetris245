7a62ca7b4f9dbc47743b5fd19fddd210dfe23a27
tetris245 <58624501+tetris245@users.noreply.github.com>
2022-09-08 01:19:16 +0200

    Update script.js

--- Quick-AccessMenu2.1/script.js
+++ Quick-AccessMenu2.1/script.js
@@ -36,10 +36,9 @@ async function NEWmenu() {
                     "<b>talking</b> = commands related to talking.\n" +
                     "<b>visual</b> = commands related to animations and background.\n" +
                     "<b>zones</b> = commands related to game zones.\n" +
-		    " \n" +
+		    "Several commands require to specify a target. It can be a real name or a member number.\n" +
 		    "Use <b>/help new</b> to get info about changes in current QAM version.\n" +
-		    " \n" +
-		    "Several commands require to specify a target. It can be a real name or a member number.</p>"	
+		    "Visit also our <a href='https://github.com/tetris245/tetris245/wiki' target='_blank'>Wiki</a></p>"	
                 );
             } else if (content.includes("bondage")) {
                 ChatRoomSendLocal(
