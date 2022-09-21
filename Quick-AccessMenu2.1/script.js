6029eff9882c58dea26e8c01cd2edf13b80fcfff
tetris245 <58624501+tetris245@users.noreply.github.com>
2022-09-21 17:51:00 +0200

    Update script.js

--- Quick-AccessMenu2.1/script.js
+++ Quick-AccessMenu2.1/script.js
@@ -8470,7 +8470,7 @@ InventoryItemNeckAccessoriesElectronicTagLoad = function() {
 function LoginRun() {
 	if (LoginCredits != null) LoginDrawCredits();
 	const CanLogin = ServerIsConnected && !LoginSubmitted;
-        DrawButton(750,120,500,60,"QAM 1.6.0 Ready!", "Pink", "Black", "");
+        DrawButton(750,120,500,60,"QAM 1.7.0 Ready!", "Pink", "Black", "");
 	DrawText(TextGet("Welcome"), 1000, 50, "White", "Black");
 	DrawText(LoginMessage, 1000, 100, "White", "Black");
 	DrawText(TextGet("AccountName"), 1000, 200, "White", "Black");
