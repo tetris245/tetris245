cbee1a4a3921319ec61810d8b2c1db73d675503b
tetris245 <58624501+tetris245@users.noreply.github.com>
2022-11-14 23:50:15 +0100

    Version 1.9.1

--- Quick-AccessMenu2.1/QAMloader.js
+++ Quick-AccessMenu2.1/QAMloader.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name Quick-AccessMenu2.1
 // @namespace https://www.bondageprojects.com/
-// @version 1.9.0
+// @version 1.9.1
 // @description Everything you'll ever need for BC
 // @author Nemesea
 // @match https://bondageprojects.elementfx.com/*
