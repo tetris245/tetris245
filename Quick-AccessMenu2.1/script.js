48468fa0069b456d5cc1682e1f3299072bffec2b
tetris245 <58624501+tetris245@users.noreply.github.com>
2022-11-20 01:00:23 +0100

    better /naked command

--- Quick-AccessMenu2.1/script.js
+++ Quick-AccessMenu2.1/script.js
@@ -3774,33 +3774,43 @@ async function NEWmenu() {
             Player.Money = content.substring(6);
             ServerPlayerSync();
         } else if (content.indexOf("/naked") == 0) {
-            var targetname = content.substring(6).trim();
-            if (targetname == undefined) {
-                targetname = Player.Name;
-            }
-            var targetfinder = new RegExp('^' + targetname + '', 'i');
-            var target = ChatRoomCharacter.filter(A => (A.Name.match(targetfinder)));
-	    if (target[0] == null) {
-                var targetnumber = parseInt(targetname);
-                target[0] = ChatRoomCharacter.find((x) => x.MemberNumber === targetnumber);              
-            }
-            if ((target[0] != null) && ((target[0] == Player) || (target[0].AllowItem == true)))  {
-		if (target[0].Nickname == '') { 
-                    tgpname = target[0].Name;
-                } else {
-                    tgpname = target[0].Nickname;
-                } 
+            if (content.endsWith("/naked")) {
                 ServerSend("ChatRoomChat", {
                     Content: "Beep",
                     Type: "Action",
                     Dictionary: [{
                         Tag: "Beep",
-                        Text: "Magical lasers make disappear the clothes on " + tgpname + " body."
+                        Text: "Magical lasers make disappear the clothes on " + tmpname + " body."
                     }]
                 });
-                CharacterNaked(target[0]);
-                ChatRoomCharacterUpdate(target[0]);
-		ChatRoomSetTarget(null);
+                CharacterNaked(Player);
+                ChatRoomCharacterUpdate(Player);  
+            } else {
+                var targetname = content.substring(6).trim();
+                var targetfinder = new RegExp('^' + targetname + '', 'i');
+                var target = ChatRoomCharacter.filter(A => (A.Name.match(targetfinder)));
+	        if (target[0] == null) {
+                    var targetnumber = parseInt(targetname);
+                    target[0] = ChatRoomCharacter.find((x) => x.MemberNumber === targetnumber);              
+                }
+                if ((target[0] != null) && (target[0].AllowItem == true))  {
+		    if (target[0].Nickname == '') { 
+                        tgpname = target[0].Name;
+                    } else {
+                        tgpname = target[0].Nickname; 
+                    } 
+                    ServerSend("ChatRoomChat", {
+                        Content: "Beep",
+                        Type: "Action",
+                        Dictionary: [{
+                            Tag: "Beep",
+                            Text: "Magical lasers make disappear the clothes on " + tgpname + " body."
+                        }]
+                    });
+                    CharacterNaked(target[0]);
+                    ChatRoomCharacterUpdate(target[0]);
+		    ChatRoomSetTarget(null);
+                }
             }
         } else if (content.indexOf("/name") == 0) {
             var NewName = content.substring(5).trim();
